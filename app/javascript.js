2299c0a49e713ecfaa04d46c0646c38a1a96c9a1
CamSchnur <42192713+CamSchnur@users.noreply.github.com>
2020-11-28 16:38:41 -0800

    overhaul layout of page using CSS grid

--- app/javascript.js
+++ app/javascript.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    applyLayout(100);
+
+
     $("#swapLight").click(function() {
         
         $('.area').find('.key').addClass('keyAltStyle');
@@ -9,4 +12,33 @@ $(document).ready(function() {
         $('.area').find('.key').removeClass('keyAltStyle');
 
     })
+    $("#layout100").click(function() {
+        removeAllLayouts();
+        addLayout(100);
+       // $('.keyboard').addClass('keyboard100');
+       // $('.keyboard').find('.tenKeyArea').removeClass('hiddenElement');
+        //$('.keyboard').find('.tenKeyArea').addClass('visibleElement');
+
+    })
+    $("#layoutTKL").click(function() {
+        removeAllLayouts();
+        addLayout('TKL');
+       // $('.keyboard').addClass('keyboardTKL');
+       // $('.tenKeyArea').removeClass('visibleElement');
+       // $('.tenKeyArea').addClass('hiddenElement');
+    })
+    function applyLayout(layout)
+    {
+        $('.keyboard').addClass('keyboard' + layout);
+    }
+    function removeLayout(layout)
+    {
+        $('.keyboard').removeClass('keyboard' + layout);
+    }
+    function removeAllLayouts()
+    {
+        removeLayout('100');
+        removeLayout('TKL');
+    }
+    
 });
